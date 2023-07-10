030dd82a03a6ccf98064bb943fa9b27796c50a50
Harsh Kedia <harshkedia717@gmail.com>
2023-07-11 00:43:23 +0530

    Bug Fix

--- src/Components/TrendingCoins.jsx
+++ src/Components/TrendingCoins.jsx
@@ -1,22 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const URL = `https://api.coingecko.com/api/v3/search/trending`;
-
-const [trending, setTrending] = useState([]);
-
-useEffect(() => {
-  axios
-    .get(URL)
-    .then((response) => {
-      setTrending(response.data.coins);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}, []);
-
 const TrendingCoins = () => {
+  const URL = `https://api.coingecko.com/api/v3/search/trending`;
+
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        setTrending(response.data.coins);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return <div>TrendingCoins</div>;
 };
 
